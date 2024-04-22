9d70bdbf7d7546ad1733d57254ef9215c96da68f
jm-316 <yoyo3302@gmail.com>
2024-04-22 16:22:30 +0900

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./pages/home/index.tsx";
+import PostPage from "./pages/posts/PostPage.tsx";
+import PostDetailPage from "./pages/posts/PostDetailPage.tsx";
+import PostEditPage from "./pages/posts/PostEditPage.tsx";
+import PostNewPage from "./pages/posts/PostNewPage.tsx";
+import LoginPage from "./pages/login/index.tsx";
+import SignupPage from "./pages/signup/index.tsx";
+import NotFoundPage from "./pages/NotFound/index.tsx";
+import ProfilePage from "./pages/profile/index.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <NotFoundPage />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "profile", element: <ProfilePage /> },
+      { path: "posts", element: <PostPage /> },
+      { path: "posts/:id", element: <PostDetailPage /> },
+      { path: "posts/new", element: <PostNewPage /> },
+      { path: "posts/edit/:id", element: <PostEditPage /> },
+      { path: "/login", element: <LoginPage /> },
+      { path: "/signup", element: <SignupPage /> },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
