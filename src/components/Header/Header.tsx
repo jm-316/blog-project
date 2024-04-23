32c19184a93f1c46980042d5e8e4e718dd69c58c
jm-316 <yoyo3302@gmail.com>
2024-04-23 11:30:14 +0900

    header 기능

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,16 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Link to="/">BLOG</Link>
+      <nav>
+        <Link to="/posts/new">글쓰기</Link>
+        <Link to="/posts">게시글</Link>
+        <Link to="/profile">프로필</Link>
+        <button>Login</button>
+      </nav>
+    </header>
+  );
 }
