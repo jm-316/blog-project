50b4865846ee3b82236859da4a65a374a4ebc15d
jm-316 <yoyo3302@gmail.com>
2024-04-24 10:42:28 +0900

    header css

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,13 +3,18 @@ import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <header>
-      <Link to="/">BLOG</Link>
+    <header className={styles.header}>
+      <Link to="/" className={styles.header__logo}>
+        BLOG
+      </Link>
       <nav>
         <Link to="/posts/new">글쓰기</Link>
-        <Link to="/posts">게시글</Link>
-        <Link to="/profile">프로필</Link>
-        <button>Login</button>
+        <Link to="/posts" className={styles.nav__link}>
+          게시글
+        </Link>
+        <Link to="/login" className={styles.nav__link}>
+          Login
+        </Link>
       </nav>
     </header>
   );
