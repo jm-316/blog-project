4440122dd4efc1aff72f80a9699fe1dc2879c06c
jm-316 <yoyo3302@gmail.com>
2024-04-24 17:40:30 +0900

    메인 페이지 css 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,51 @@
 import { Link } from "react-router-dom";
-import "./Home.module.css";
+import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.post__list}>
       {[...Array(10)].map((post, index) => (
-        <div>
-          <Link to={`/posts/${index}`}>
-            <div>
-              <div>프로필</div>
-              <div>test@test.com</div>
-              <div>2024-04-24</div>
+        <div className={styles.post__box}>
+          <div className={styles.post__box__header}>
+            <Link to={`/posts/${index}`}>
+              <div className={styles.post__profile__box}>
+                <div className={styles.post__profile} />
+                <div className={styles.post__author__email}>test@test.com</div>
+                <div className={styles.post__date}>2024-04-24</div>
+              </div>
+            </Link>
+            <div className={styles.post__utils__box}>
+              <div className={styles.post__delete}>삭제</div>
+              <div className={styles.post__edit}>수정</div>
             </div>
-            <div>게시글 {index}</div>
-          </Link>
-          <div>
-            <div>삭제</div>
-            <div>수정</div>
           </div>
+          <Link to={`/posts/${index}`}>
+            <div className={styles.post__title}>게시글 {index}</div>
+            <div className={styles.post__text}>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+              culpa distinctio placeat minus veritatis, perspiciatis rerum eos
+              saepe aut molestias quis voluptatem dicta quaerat autem blanditiis
+              fuga pariatur quos laudantium! Lorem ipsum dolor sit amet
+              consectetur, adipisicing elit. Eligendi culpa distinctio placeat
+              minus veritatis, perspiciatis rerum eos saepe aut molestias quis
+              voluptatem dicta quaerat autem blanditiis fuga pariatur quos
+              laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Eligendi culpa distinctio placeat minus veritatis,
+              perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
+              quaerat autem blanditiis fuga pariatur quos laudantium! Lorem
+              ipsum dolor sit amet consectetur, adipisicing elit. Eligendi culpa
+              distinctio placeat minus veritatis, perspiciatis rerum eos saepe
+              aut molestias quis voluptatem dicta quaerat autem blanditiis fuga
+              pariatur quos laudantium! Lorem ipsum dolor sit amet consectetur,
+              adipisicing elit. Eligendi culpa distinctio placeat minus
+              veritatis, perspiciatis rerum eos saepe aut molestias quis
+              voluptatem dicta quaerat autem blanditiis fuga pariatur quos
+              laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
+              elit. Eligendi culpa distinctio placeat minus veritatis,
+              perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
+              quaerat autem blanditiis fuga pariatur quos laudantium!
+            </div>
+          </Link>
         </div>
       ))}
     </div>
