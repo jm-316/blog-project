defb902f0ac651fe55b50b59c20d1024341ae1ab
jm-316 <yoyo3302@gmail.com>
2024-04-24 17:44:36 +0900

    게시글 navigation 구조 작업

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,51 +3,60 @@ import styles from "./Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.post__list}>
-      {[...Array(10)].map((post, index) => (
-        <div className={styles.post__box}>
-          <div className={styles.post__box__header}>
+    <>
+      <div>
+        <div>전체보기</div>
+        <div>나의 글</div>
+      </div>
+      <div className={styles.post__list}>
+        {[...Array(10)].map((post, index) => (
+          <div className={styles.post__box}>
+            <div className={styles.post__box__header}>
+              <Link to={`/posts/${index}`}>
+                <div className={styles.post__profile__box}>
+                  <div className={styles.post__profile} />
+                  <div className={styles.post__author__email}>
+                    test@test.com
+                  </div>
+                  <div className={styles.post__date}>2024-04-24</div>
+                </div>
+              </Link>
+              <div className={styles.post__utils__box}>
+                <div className={styles.post__delete}>삭제</div>
+                <div className={styles.post__edit}>수정</div>
+              </div>
+            </div>
             <Link to={`/posts/${index}`}>
-              <div className={styles.post__profile__box}>
-                <div className={styles.post__profile} />
-                <div className={styles.post__author__email}>test@test.com</div>
-                <div className={styles.post__date}>2024-04-24</div>
+              <div className={styles.post__title}>게시글 {index}</div>
+              <div className={styles.post__text}>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eligendi culpa distinctio placeat minus veritatis, perspiciatis
+                rerum eos saepe aut molestias quis voluptatem dicta quaerat
+                autem blanditiis fuga pariatur quos laudantium! Lorem ipsum
+                dolor sit amet consectetur, adipisicing elit. Eligendi culpa
+                distinctio placeat minus veritatis, perspiciatis rerum eos saepe
+                aut molestias quis voluptatem dicta quaerat autem blanditiis
+                fuga pariatur quos laudantium! Lorem ipsum dolor sit amet
+                consectetur, adipisicing elit. Eligendi culpa distinctio placeat
+                minus veritatis, perspiciatis rerum eos saepe aut molestias quis
+                voluptatem dicta quaerat autem blanditiis fuga pariatur quos
+                laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
+                elit. Eligendi culpa distinctio placeat minus veritatis,
+                perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
+                quaerat autem blanditiis fuga pariatur quos laudantium! Lorem
+                ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
+                culpa distinctio placeat minus veritatis, perspiciatis rerum eos
+                saepe aut molestias quis voluptatem dicta quaerat autem
+                blanditiis fuga pariatur quos laudantium! Lorem ipsum dolor sit
+                amet consectetur, adipisicing elit. Eligendi culpa distinctio
+                placeat minus veritatis, perspiciatis rerum eos saepe aut
+                molestias quis voluptatem dicta quaerat autem blanditiis fuga
+                pariatur quos laudantium!
               </div>
             </Link>
-            <div className={styles.post__utils__box}>
-              <div className={styles.post__delete}>삭제</div>
-              <div className={styles.post__edit}>수정</div>
-            </div>
           </div>
-          <Link to={`/posts/${index}`}>
-            <div className={styles.post__title}>게시글 {index}</div>
-            <div className={styles.post__text}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-              culpa distinctio placeat minus veritatis, perspiciatis rerum eos
-              saepe aut molestias quis voluptatem dicta quaerat autem blanditiis
-              fuga pariatur quos laudantium! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi culpa distinctio placeat
-              minus veritatis, perspiciatis rerum eos saepe aut molestias quis
-              voluptatem dicta quaerat autem blanditiis fuga pariatur quos
-              laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi culpa distinctio placeat minus veritatis,
-              perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
-              quaerat autem blanditiis fuga pariatur quos laudantium! Lorem
-              ipsum dolor sit amet consectetur, adipisicing elit. Eligendi culpa
-              distinctio placeat minus veritatis, perspiciatis rerum eos saepe
-              aut molestias quis voluptatem dicta quaerat autem blanditiis fuga
-              pariatur quos laudantium! Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Eligendi culpa distinctio placeat minus
-              veritatis, perspiciatis rerum eos saepe aut molestias quis
-              voluptatem dicta quaerat autem blanditiis fuga pariatur quos
-              laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi culpa distinctio placeat minus veritatis,
-              perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
-              quaerat autem blanditiis fuga pariatur quos laudantium!
-            </div>
-          </Link>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </>
   );
 }
