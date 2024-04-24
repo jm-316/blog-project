5d6582c27fcbaed90b524e598be27234113cc58e
jm-316 <yoyo3302@gmail.com>
2024-04-24 15:51:58 +0900

    메인 페이지 임시 구조

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,25 @@
+import { Link } from "react-router-dom";
+import "./Home.module.css";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      {[...Array(10)].map((post, index) => (
+        <div>
+          <Link to={`/posts/${index}`}>
+            <div>
+              <div>프로필</div>
+              <div>test@test.com</div>
+              <div>2024-04-24</div>
+            </div>
+            <div>게시글 {index}</div>
+          </Link>
+          <div>
+            <div>삭제</div>
+            <div>수정</div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
