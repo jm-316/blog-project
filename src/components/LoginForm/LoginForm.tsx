fce7c5d1304d6d02c4371362129adb3b46511f80
jm-316 <yoyo3302@gmail.com>
2024-04-25 16:25:38 +0900

    css 작업

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import styles from "./LoginForm.module.css";
 
 export default function LoginForm() {
   return (
-    <form>
-      <h1>로그인</h1>
+    <form className={styles.form}>
+      <h1 className={styles.form__title}>로그인</h1>
       <div>
         <label htmlFor="email">이메일</label>
         <input type="email" name="email" id="email" required />
@@ -12,11 +13,13 @@ export default function LoginForm() {
         <label htmlFor="password">비밀번호</label>
         <input type="password" name="password" id="password" required />
       </div>
-      <div>
+      <div className={styles.form__block}>
         계정이 없으신가요?
-        <Link to="/signup">회원가입하기</Link>
+        <Link to="/signup" className={styles.form__link}>
+          회원가입하기
+        </Link>
       </div>
-      <button>로그인</button>
+      <button className={styles.form__btn}>로그인</button>
     </form>
   );
 }
