e3c51e43d65005a5f08709073a2e170d7636538b
jm-316 <yoyo3302@gmail.com>
2024-04-26 16:21:43 +0900

    css 작업

--- src/components/PostForm/PostForm.tsx
+++ src/components/PostForm/PostForm.tsx
@@ -1,27 +1,31 @@
 import { CATEGORIES } from "../Category/Category.types";
+import styles from "./PostFrom.module.css";
 
 export default function PostForm() {
   return (
-    <form>
-      <div>
-        <label htmlFor="title">제목</label>
-        <input type="text" name="title" id="title" required />
-      </div>
-      <div>
-        <label htmlFor="category">카테고리</label>
-        <select>
-          {CATEGORIES.map((category) => (
-            <option value={category} key={category}>
-              {category}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div>
-        <label htmlFor="content">내용</label>
-        <textarea name="content" id="content" required />
-      </div>
-      <button>제출</button>
-    </form>
+    <div className={styles.post__wrapper}>
+      <h1 className={styles.post__title}>게시글 등록</h1>
+      <form className={styles.post__form}>
+        <div className={styles.post__block}>
+          <label htmlFor="title">제목</label>
+          <input type="text" name="title" id="title" required />
+        </div>
+        <div className={styles.post__block}>
+          <label htmlFor="category">카테고리</label>
+          <select>
+            {CATEGORIES.map((category) => (
+              <option value={category} key={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className={styles.post__block}>
+          <label htmlFor="content">내용</label>
+          <textarea name="content" id="content" required />
+        </div>
+        <button className={styles.post__btn}>제출</button>
+      </form>
+    </div>
   );
 }
