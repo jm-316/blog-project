85c4a02976169c6dff9a699e9a957ada3c9a23bd
jm-316 <yoyo3302@gmail.com>
2024-04-26 10:06:15 +0900

    auth 관련 추가 및 vite에 맞게 수정

--- src/firebaseApp.ts
+++ src/firebaseApp.ts
@@ -1,14 +1,24 @@
-import { initializeApp } from "firebase/app";
+import { initializeApp, FirebaseApp, getApp } from "firebase/app";
+import "firebase/auth";
+
+export let app: FirebaseApp;
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_API_ID,
+  apiKey: import.meta.env.VITE_APP_API_KEY,
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_API_ID,
 };
 
-const app = initializeApp(firebaseConfig);
+try {
+  app = getApp("app");
+} catch (error) {
+  app = initializeApp(firebaseConfig, "app");
+}
 
-export default app;
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+
+export default firebase;
