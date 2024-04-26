4c449c4f6f5d5fd5e6e35d7048da69851ab892b2
jm-316 <yoyo3302@gmail.com>
2024-04-26 15:06:41 +0900

    loginform 컴포넌트 수정

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,12 +1,18 @@
 import { Link, useNavigate } from "react-router-dom";
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 import React, { useState } from "react";
 import { app } from "../../firebaseApp";
+import { LoginFormProps } from "./LoginForm.types";
 import styles from "./LoginForm.module.css";
 
-export default function LoginForm() {
+export default function LoginForm({ isSignup }: LoginFormProps) {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
+  const [passwordConfirm, setPasswordConfirm] = useState<string>("");
   const [error, setError] = useState<string>("");
 
   const navigate = useNavigate();
@@ -36,6 +42,22 @@ export default function LoginForm() {
           ? ""
           : "비밀번호는 영문 대소문자, 숫자를 혼합하여 8~20자로 입력해주세요.."
       );
+
+      if (passwordConfirm?.length > 0 && value !== passwordConfirm) {
+        setError("비밀번호와 값이 다릅니다. 다시 확인해주세요");
+      }
+    }
+
+    if (name === "password_confirm") {
+      setPasswordConfirm(value);
+
+      const isValidPasswordConfirm = value !== password;
+
+      setError(
+        isValidPasswordConfirm
+          ? "비밀번호와 값이 다릅니다. 다시 확인해주세요"
+          : ""
+      );
     }
   };
 
@@ -45,15 +67,21 @@ export default function LoginForm() {
     try {
       const auth = getAuth(app);
 
-      await signInWithEmailAndPassword(auth, email, password);
-      navigate("/");
+      if (isSignup) {
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        navigate("/");
+      } else {
+        await signInWithEmailAndPassword(auth, email, password);
+        navigate("/");
+      }
     } catch (error) {
       console.log(error);
     }
   };
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
-      <h1 className={styles.form__title}>로그인</h1>
+      <h1 className={styles.form__title}>{isSignup ? "회원가입" : "로그인"}</h1>
       <div>
         <label htmlFor="email">이메일</label>
         <input
@@ -76,18 +104,42 @@ export default function LoginForm() {
           required
         />
       </div>
+      {isSignup && (
+        <div>
+          <label htmlFor="password">비밀번호 확인</label>
+          <input
+            type="password"
+            name="password_confirm"
+            id="password_confirm"
+            value={passwordConfirm}
+            onChange={handleChange}
+            required
+          />
+        </div>
+      )}
       {error && error.length > 0 && (
         <div className={styles.form__block}>
           <div className={styles.form__error}>{error}</div>
         </div>
       )}
-      <div className={styles.form__block}>
-        계정이 없으신가요?
-        <Link to="/signup" className={styles.form__link}>
-          회원가입하기
-        </Link>
-      </div>
-      <button className={styles.form__btn}>로그인</button>
+      {isSignup ? (
+        <div className={styles.form__block}>
+          계정이 이미 있으신가요?
+          <Link to="/login" className={styles.form__link}>
+            로그인하기
+          </Link>
+        </div>
+      ) : (
+        <div className={styles.form__block}>
+          계정이 없으신가요?
+          <Link to="/signup" className={styles.form__link}>
+            회원가입하기
+          </Link>
+        </div>
+      )}
+      <button className={styles.form__btn}>
+        {isSignup ? "회원가입" : "로그인"}
+      </button>
     </form>
   );
 }
