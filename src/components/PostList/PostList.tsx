ab4d4b55484120e19a95c7a97f12c45a3e4dcd5c
jm-316 <yoyo3302@gmail.com>
2024-04-29 10:42:25 +0900

    main 페이지에서 post 불러오기

--- src/components/PostList/PostList.tsx
+++ src/components/PostList/PostList.tsx
@@ -1,53 +1,60 @@
 import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import AuthContext from "../../context/AuthContext";
+import Category from "../Category/Category";
+import UseGetPosts from "../../hooks/UseGetPosts";
+import { CategoryType, PostListProps, TabType } from "../../typings/post.types";
 import styles from "./PostList.module.css";
 
-export default function PostList() {
+export default function PostList({ defaultTab = "all" }: PostListProps) {
+  const [activeTab, setActiveTab] = useState<TabType | CategoryType>(
+    defaultTab
+  );
+
+  const { user } = useContext(AuthContext);
+  const posts = UseGetPosts(activeTab, user);
+
+  const handleChangeActiveTab = (tab: TabType | CategoryType) => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div className={styles.post__list}>
-      {[...Array(10)].map((post, index) => (
-        <div className={styles.post__box} key={index}>
-          <div className={styles.post__box__header}>
-            <Link to={`/posts/${index}`}>
-              <div className={styles.post__profile__box}>
-                <div className={styles.post__profile} />
-                <div className={styles.post__author__email}>test@test.com</div>
-                <div className={styles.post__date}>2024-04-24</div>
+    <>
+      <Category
+        activeTab={activeTab}
+        handleChangeActiveTab={handleChangeActiveTab}
+      />
+      <div className={styles.post__list}>
+        {posts.length > 0 ? (
+          posts.map((post) => (
+            <div className={styles.post__box} key={post?.id}>
+              <div className={styles.post__box__header}>
+                <Link to={`/posts/${post?.id}`}>
+                  <div className={styles.post__profile__box}>
+                    <div className={styles.post__profile} />
+                    <div className={styles.post__author__email}>
+                      {post?.email}
+                    </div>
+                    <div className={styles.post__date}>{post?.createdAt}</div>
+                  </div>
+                </Link>
+                {post?.email === user?.email && (
+                  <div className={styles.post__utils__box}>
+                    <div className={styles.post__delete}>삭제</div>
+                    <div className={styles.post__edit}>수정</div>
+                  </div>
+                )}
               </div>
-            </Link>
-            <div className={styles.post__utils__box}>
-              <div className={styles.post__delete}>삭제</div>
-              <div className={styles.post__edit}>수정</div>
+              <Link to={`/posts/${post?.id}`}>
+                <div className={styles.post__title}>{post?.title}</div>
+                <div className={styles.post__text}>{post?.content}</div>
+              </Link>
             </div>
-          </div>
-          <Link to={`/posts/${index}`}>
-            <div className={styles.post__title}>게시글 {index}</div>
-            <div className={styles.post__text}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eligendi
-              culpa distinctio placeat minus veritatis, perspiciatis rerum eos
-              saepe aut molestias quis voluptatem dicta quaerat autem blanditiis
-              fuga pariatur quos laudantium! Lorem ipsum dolor sit amet
-              consectetur, adipisicing elit. Eligendi culpa distinctio placeat
-              minus veritatis, perspiciatis rerum eos saepe aut molestias quis
-              voluptatem dicta quaerat autem blanditiis fuga pariatur quos
-              laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi culpa distinctio placeat minus veritatis,
-              perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
-              quaerat autem blanditiis fuga pariatur quos laudantium! Lorem
-              ipsum dolor sit amet consectetur, adipisicing elit. Eligendi culpa
-              distinctio placeat minus veritatis, perspiciatis rerum eos saepe
-              aut molestias quis voluptatem dicta quaerat autem blanditiis fuga
-              pariatur quos laudantium! Lorem ipsum dolor sit amet consectetur,
-              adipisicing elit. Eligendi culpa distinctio placeat minus
-              veritatis, perspiciatis rerum eos saepe aut molestias quis
-              voluptatem dicta quaerat autem blanditiis fuga pariatur quos
-              laudantium! Lorem ipsum dolor sit amet consectetur, adipisicing
-              elit. Eligendi culpa distinctio placeat minus veritatis,
-              perspiciatis rerum eos saepe aut molestias quis voluptatem dicta
-              quaerat autem blanditiis fuga pariatur quos laudantium!
-            </div>
-          </Link>
-        </div>
-      ))}
-    </div>
+          ))
+        ) : (
+          <div className={styles.post__noPost}>게시글이 없습니다. </div>
+        )}
+      </div>
+    </>
   );
 }
