d4181dc5ca4363df3615a1df42c1b1fef3c27c6f
jm-316 <yoyo3302@gmail.com>
2024-04-29 18:15:52 +0900

    css 작업

--- src/components/PostDetail/PostDetail.tsx
+++ src/components/PostDetail/PostDetail.tsx
@@ -1,20 +1,26 @@
+import styles from "./PostDetail.module.css";
+
 export default function PostDetail() {
   return (
-    <div>
-      <div>
-        <div>title</div>
-        <div>
-          <div>profile</div>
-          <div>email</div>
-          <div>2024-04-29</div>
+    <div className={styles.post__detail}>
+      <div className={styles.post__box}>
+        <div className={styles.post__title}>title</div>
+        <div className={styles.post__profile__box}>
+          <div className={styles.post__profile} />
+          <div className={styles.post__author__email}>email</div>
+          <div className={styles.post__date}>2024-04-29</div>
         </div>
       </div>
-      <div>
-        <div>category</div>
-        <div>delete</div>
-        <div>edit</div>
+      <div className={styles.post__utils__box}>
+        <div className={styles.post__category}>category</div>
+        <div className={styles.post__edit__box}>
+          <div className={styles.post__delete}>delete</div>
+          <div className={styles.post__edit}>edit</div>
+        </div>
       </div>
-      <div>content</div>
+      <div className={`${styles.post__text} ${styles.post__text__preWrap}`}>
+        content
+      </div>
     </div>
   );
 }
