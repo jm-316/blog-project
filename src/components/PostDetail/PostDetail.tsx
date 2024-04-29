9e9a738081f84426ce7544d795e8df5ebd714da5
jm-316 <yoyo3302@gmail.com>
2024-04-29 18:22:02 +0900

    post 불러오기

--- src/components/PostDetail/PostDetail.tsx
+++ src/components/PostDetail/PostDetail.tsx
@@ -1,25 +1,37 @@
+import { useEffect, useState } from "react";
 import styles from "./PostDetail.module.css";
+import { PostProps } from "../../typings/post.types";
+import { useParams } from "react-router";
+import { getPost } from "../../firebaseApp";
 
 export default function PostDetail() {
+  const [post, setPost] = useState<PostProps | null>(null);
+
+  const params = useParams();
+
+  useEffect(() => {
+    if (params?.id) getPost(params?.id, setPost);
+  }, [params?.id]);
+
   return (
     <div className={styles.post__detail}>
       <div className={styles.post__box}>
-        <div className={styles.post__title}>title</div>
+        <div className={styles.post__title}>{post?.title}</div>
         <div className={styles.post__profile__box}>
           <div className={styles.post__profile} />
-          <div className={styles.post__author__email}>email</div>
-          <div className={styles.post__date}>2024-04-29</div>
+          <div className={styles.post__author__email}>{post?.email}</div>
+          <div className={styles.post__date}>{post?.createdAt}</div>
         </div>
       </div>
       <div className={styles.post__utils__box}>
-        <div className={styles.post__category}>category</div>
+        <div className={styles.post__category}>{post?.category}</div>
         <div className={styles.post__edit__box}>
           <div className={styles.post__delete}>delete</div>
           <div className={styles.post__edit}>edit</div>
         </div>
       </div>
       <div className={`${styles.post__text} ${styles.post__text__preWrap}`}>
-        content
+        {post?.content}
       </div>
     </div>
   );
