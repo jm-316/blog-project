a25901afb83482d6082950c3a5901ce59d02654b
jm-316 <yoyo3302@gmail.com>
2024-05-06 14:02:02 +0900

    router 방식 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { Outlet } from "react-router";
 import { useContext } from "react";
 import Header from "./components/Header/Header";
 import ThemeContext from "./context/ThemeContext";
 import ThemeButton from "./components/ThemeButton/ThemeButton";
+import Router from "./components/Router/Router";
 
 function App() {
   const context = useContext(ThemeContext);
   return (
     <div className={`${context.theme === "light" ? "white" : "dark"}`}>
       <Header />
-      <Outlet />
+      <Router />
       <ThemeButton />
     </div>
   );
---
+++ src/components/Router/Router.tsx
@@ -0,0 +1,45 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "../../pages/home";
+import ProtectedRoute from "../ProtectedRoute/ProtectedRoute";
+import ProfilePage from "../../pages/profile";
+import PostDetailPage from "../../pages/posts/PostDetailPage";
+import PostNewPage from "../../pages/posts/PostNewPage";
+import PostEditPage from "../../pages/posts/PostEditPage";
+import LoginPage from "../../pages/login";
+import SignupPage from "../../pages/signup";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route
+        path="/profile"
+        element={
+          <ProtectedRoute>
+            <ProfilePage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/posts/:id" element={<PostDetailPage />} />
+      <Route
+        path="/posts/new"
+        element={
+          <ProtectedRoute>
+            <PostNewPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/posts/edit/:id" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <ProtectedRoute>
+            <PostEditPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+    </Routes>
+  );
+}
