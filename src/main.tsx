e95e503d87043b2041e3980b217b6b94afac2b36
jm-316 <yoyo3302@gmail.com>
2024-07-27 11:45:03 +0900

    queryClinet 수정

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router } from "react-router-dom";
-import { QueryClientProvider, useQueryClient } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
-const queryClient = useQueryClient();
+const queryClient = new QueryClient();
 
 root.render(
   <ThemeContextProvider>
